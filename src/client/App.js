a54006b9951123124dec437efd286f04c992c926
surenderhsr44 <suenderhsr44@gmail.com>
2018-09-06 15:43:37 +0530

    create app using webpack 3 or old version and without React app

---
+++ src/client/App.js
@@ -0,0 +1,12 @@
+
+import React from 'react';
+
+const App = () => <div>💅
+<p>We’ve created a step-by-step guide of how to server side render a React application with styled-components. There are no bells or whistles around this guide because we wanted to focus on the core concepts. From here, you can use these principles in your existing apps or build on top of this guide to create a more complex app. There are other guides that will help you piece together how to add state management, routing, performance improvements, and more.
+
+Don’t stop learning!
+Thank you for following this guide and reading through to the end. Hopefully it helped you understand and get started with React/SSR and styled-components. If you know anyone that would benefit from this guide I would love if you recommended it to them!
+
+If you’d like to see a larger code base that’s server side rendered using styled-components you can check out one of my projects, Jobeir, on Github. On top of that, the styled-components documentation is always a good place to go.</p></div>;
+
+export default App;
